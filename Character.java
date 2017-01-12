f002007f057c46705203a197e102187bb73adf06
Kin Gin <passingbyhere>
2017-01-11 23:24:43 -0500

    Added Character class and developement file.

---
+++ Character.java
@@ -0,0 +1,50 @@
+public class Character {
+
+    //instance vars
+    protected String name;
+    protected int health;
+    final protected int F_HEALTH;
+    protected int mvtRange;
+    protected int atkRange;
+    protected int defense;
+    final protected int F_DEF;
+    protected int attack;
+    final protected int F_ATK;
+    protected int teamOrder;
+    protected boolean alive;
+    
+    public Character(String name, int HP
+    
+    public void move(int x, int y, ArrayList a){
+        //if x < 0, left.....if x > 0, right
+        //if y < 0, down.....if y > 0, up
+    }
+    
+    public void lowerHP(idk){
+    }
+    
+    public int getHealth(){
+        return health;
+    }
+    
+    public int getDef(){
+        return defense;
+    }
+    
+    public int getOrder(){
+        return teamOrder;
+    }
+    
+    public boolean isAlive(){
+        return alive;
+    }
+    
+    public int getmvtRange(){
+        return mvtRangel
+    }
+    
+    public int getatkRange(){
+        return atkRange;
+    }
+
+}
