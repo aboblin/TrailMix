e280a3077194636ce9d71485a75021147981314c
v-law <vlawren@stuy.edu>
2017-01-12 23:09:00 -0500

    Create Monster.java
    added default constructor, methods

---
+++ Monster.java
@@ -0,0 +1,26 @@
+public class Monster extends Character{
+
+    public Monster(){
+        super("defaultMonster", 150, 2, 1, 20, 20+ (int) Math.random()*45, 0);
+    }
+
+    public Monster(String Monsternum){
+        super(Monsternum, 150, 2, 1, 20, 20+ (int) Math.random()*45, 0);
+    }
+    
+    public String about(){
+        return "How you gon' fight fire with fire?";
+    }
+    
+    public void specialatk(){
+        
+    }
+    
+    //normalize: prepares monster to perform a normal attack
+    //sets defense to default value 40
+    //sets strength to default value 100
+    public void normalatk(){
+        
+    }
+    
+}
