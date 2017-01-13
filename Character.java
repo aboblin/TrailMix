5b26ce227931e1c8b3578ac9aabc52e3cd00869f
v-law <vlawren@stuy.edu>
2017-01-12 23:07:49 -0500

    Update Character.java
    added default constructor, methods for superclass, some instance vars.

--- Character.java
+++ Character.java
@@ -3,24 +3,41 @@ public class Character {
     //instance vars
     protected String name;
     protected int health;
-    final protected int F_HEALTH;
+    final protected int F_HEALTH; //full health
     protected int mvtRange;
     protected int atkRange;
     protected int defense;
-    final protected int F_DEF;
+    final protected int F_DEF; //full defense
     protected int attack;
-    final protected int F_ATK;
+    final protected int F_ATK; //full attack
     protected int teamOrder;
-    protected boolean alive;
     
-    public Character(String name, int HP
-    
-    public void move(int x, int y, ArrayList a){
-        //if x < 0, left.....if x > 0, right
-        //if y < 0, down.....if y > 0, up
+    public Character(String nam, int hp, int mvt, int atkrang, int defrang, int atk, int teamOdr){
+        name = nam;
+        health = hp;
+        mvtRange = mvt;
+        atkRange = atkrang;
+        defense = defrang;
+        attack = atk;
+        teamOrder = teamOdr;
     }
     
-    public void lowerHP(idk){
+    public boolean isAlive(){
+        return health > 0;
+    }
+    
+    public void move(int x, int y, Object[][] a){
+        //use coordinates from the 2D ArrayList to input
+        
+    }
+    
+    public void attack(int x, int y, Object[][] a){
+        //use coordinates from the 2D ArrayList to input
+    
+    }
+    
+    public void lowerHP(int healthLost){
+        health -= healthLost;
     }
     
     public int getHealth(){
@@ -35,10 +52,6 @@ public class Character {
         return teamOrder;
     }
     
-    public boolean isAlive(){
-        return alive;
-    }
-    
     public int getmvtRange(){
         return mvtRangel
     }
@@ -46,5 +59,14 @@ public class Character {
     public int getatkRange(){
         return atkRange;
     }
+    
+    //normalize method
+    public abstract void normalize();
 
+    //specialize method
+    public abstract void specialize();
+    
+    //about method
+    public abstract String about();
+    
 }
