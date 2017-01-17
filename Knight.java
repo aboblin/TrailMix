4ca75d69b8e03e9a6f44f6d3350b620b64cdee86
v-law <vlawren@stuy.edu>
2017-01-16 22:40:34 -0500

    Create Knight.java
    default constructor, about, special and normal attacks

---
+++ Knight.java
@@ -0,0 +1,25 @@
+public class Knight extends User {
+
+    //constructor
+    public Knight(String newName) {
+        super("Knight", 150, 3, 5, 20, 20+ (int) Math.random()*45, 1);
+        //name, health, mvtrange, atkrange, defense, attack, teamOrder
+    }
+    
+    public String about() {
+        return "Knights have a wider movement range, but can only attack those close to them. Their attacks are very powerful.";
+    }
+    
+    public void specialatkk() {
+       defense -= 20;
+       attack += 20;
+    }
+
+    //normalatkk: prepares the Knight to perform a normal attack
+    //sets defense to default value
+    //sets strength to default value
+    public void normalatkk() {
+        defense = F_DEFENSE;
+        strength = F_ATTACK;
+    }
+}
