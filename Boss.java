a23838027b919b7bbd001c0ff0f30afaaeec4302
v-law <vlawren@stuy.edu>
2017-01-16 22:49:45 -0500

    commented out special attack

--- Boss.java
+++ Boss.java
@@ -9,12 +9,12 @@ public class Boss extends Monster {
     public String about() {
         return "You can't fight as a boss. The fules of the universe won't allow for it.";
     }
-    
+/*-----------------------------------
     public void specialatkk() {
        defense -= 30;
        attack += 30;
     }
-
+-----------------------------------*/
     //normalatkk: prepares the Archer to perform a normal attack
     //sets defense to default value 40
     //sets strength to default value 100
