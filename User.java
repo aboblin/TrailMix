4243e5b4c6d684bdd526f09d88fe496fe8aef010
passingbyhere <echung@stuy.edu>
2017-01-18 22:17:42 -0500

    Made user abstract and made some methods abstract

--- User.java
+++ User.java
@@ -1,5 +1,5 @@
-public class User extends Character{
-
+public abstract class User extends Character{
+    /*------------------------------------
     public User(){
         super("defaultUser", 150, 2, 1, 20, 20+ (int) Math.random()*45, 0);
     }
@@ -7,16 +7,13 @@ public class User extends Character{
     public User(String Usernum){
         super(Usernum, 150, 2, 1, 20, 20+ (int) Math.random()*45, 0);
     }
-    
-    public void specialatk(){
-        
+    --------------------------------------*/
+    public String getName(){
+        return name;
     }
-    
-    //normalize: prepares the Warrior to perform a normal attack
-    //sets defense to default value 40
-    //sets strength to default value 100
-    public void normalatk(){
-        
-    }
+    public abstract void heal();
+    public abstract void specialize();
+    public abstract void normalize();
+    public abstract String about();
     
 }
