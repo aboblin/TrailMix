092e9f80812ba712864dfa8029c0447525d9d45a
aboblin <aboylan@stuy.edu>
2017-01-22 20:38:08 -0500

    Update Character.java

--- Character.java
+++ Character.java
@@ -4,57 +4,12 @@ public abstract class Character {
 
     //instance vars
     protected String name;
-    protected int health;
+    protected int health, mvtRange, atkRange, attack, defense, teamOrder, currentX, currentY;
     final protected int F_HEALTH; //full health
-    protected int mvtRange;
-    protected int atkRange;
-    protected int defense;
     final protected int F_DEF; //full defense
-    protected int attack;
     final protected int F_ATK; //full attack
-    protected int teamOrder;
-    protected int currentX;
-    protected int currentY;
-    /*---------------------------------
-    public Character(String nam, int hp, int mvt, int atkrang, int defrang, int atk, int teamOdr){
-        name = nam;
-        health = hp;
-        F_HEALTH = hp;
-        mvtRange = mvt;
-        atkRange = atkrang;
-        defense = defrang;
-        F_DEF = defrang;
-        attack = atk;
-        F_ATK = atk;
-        teamOrder = teamOdr;
-    }
-    ----------------------------------*/
-    public boolean isAlive(){
-        return health > 0;
-    }
-    
-    public void setCoordinate(int x, int y){
-        currentX = x;
-        currentY = y;
-    }
-    
-    public void move(int x, int y, ArrayList[][] a){
-        //use coordinates from the 2D ArrayList to input
-        ArrayList placement = a[x][y];
-        a[x][y] = a[currentX][currentY];
-        a[currentX][currentY] = placement;
-        setCoordinate(x, y);
-    }
-    
-    public void attack(int x, int y, ArrayList[][] a){
-        //use coordinates from the 2D ArrayList to input
-    
-    }
-    
-    public void lowerHP(int healthLost){
-        health -= healthLost;
-    }
-    
+	
+	//Accessor Methods	
     public int getHealth(){
         return health;
     }
@@ -74,14 +29,49 @@ public abstract class Character {
     public int getatkRange(){
         return atkRange;
     }
+	
+	//Mutator Methods	
+	public int setDef(int Def){
+		defense = Def;
+	}
     
-    //normalize method
-    public abstract void normalize();
+	public int setAtk(int Atk){
+		attack = Atk;
+	}	
+	
+	public int setOrder(int placement){
+		teamOrder = placement;
+	}
+	
+	//Other Methods
 
-    //specialize method
-    public abstract void specialize();
+    public boolean isAlive(){
+        return health > 0;
+    }
     
-    //about method
+    public void setCoordinate(int x, int y){
+        currentX = x;
+        currentY = y;
+    }
+    
+    public void move(int x, int y, Object[][] a){
+        //use coordinates from the 2D ArrayList to input
+        ArrayList placement = a[x][y];
+        a[x][y] = a[currentX][currentY];
+        a[currentX][currentY] = placement;
+        setCoordinate(x, y);
+    }
+    
+    public void attack(Character c){
+        //use coordinates from the 2D ArrayList to input
+    	c.lowerHP(attack);
+    }
+    
+    public void lowerHP(int healthLost){
+        health -= healthLost;
+    }
+
+    //about method (abstract)
     public abstract String about();
     
 }
